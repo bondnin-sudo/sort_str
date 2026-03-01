5213b748c221bb51f2d64738575494cf602a5948
Ian Bond <257003848+bondnin-sudo@users.noreply.github.com>
2026-03-01 23:52:55 +0000

    solved sort_str and added instructions

---
+++ index.js
@@ -0,0 +1,6 @@
+function sortByLength (array) {
+  // Return an array containing the same strings,
+  // ordered from shortest to longest
+  array.sort( (a, b) => a.length - b.length)
+    return array;
+}
